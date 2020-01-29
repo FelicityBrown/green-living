d898869ff2f8e591b397aca9b46135234b6f7612
Felicity <felicitygbrown@gmail.com>
2020-01-29 20:48:09 +0000

    Added base.js file to correct mobile view burger menu

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -358,16 +358,6 @@ function init() {
     initMap()
   }
     
-  document.getElementById('hamburger').onclick = function() {
-    document.body.classList.add('has-open-mobile-nav')
-  }
-  document.getElementById('close-hamburger').onclick = function() {
-    document.body.classList.remove('has-open-mobile-nav')
-  }
-  document.getElementById('js-page-mask').onclick = function() {
-    document.body.classList.remove('has-open-mobile-nav')
-  }
-
   var xhr = new XMLHttpRequest();
 
   xhr.open('GET', '/places.json')
