0b5f44702179ba6e2dd7e8ac64c5b88d4e9e6d26
Damian Moore <damian@epixstudios.co.uk>
2021-01-31 11:41:37 +0000

    Updates map tiles

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -219,13 +219,12 @@ function initMap() {
     mymap = L.map('mapdesktop').setView(initialCoordinatesDesktop, initialZoom);
   }
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}{r}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
     maxZoom: 25,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-  }).addTo(mymap);
+    attribution:
+      '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    id: 'mapbox.streets',
+  }).addTo(mymap)
 
   markerGroup = L.layerGroup().addTo(mymap);
 }
